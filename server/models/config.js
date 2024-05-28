e93ec6cc4b01dc45f23f404efc97e7958cc9c0a8
khyati jethava <khyati_jethava@kainest.com>
2024-05-28 18:12:27 +0530

    test code

--- server/models/config.js
+++ server/models/config.js
@@ -1,27 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-require('dotenv').config();
-const uri = process.env.DATABASE_URL;
+require("dotenv").config();
+const uri = process.env.MONGODB_URI;
 
 const options = {
-    serverSelectionTimeoutMS: 10000,
+  serverSelectionTimeoutMS: 10000,
 };
 
 // Connect to MongoDB
-mongoose.connect(uri, options).then(() => {
+mongoose
+  .connect(uri, options)
+  .then(() => {
     console.log("Connected to MongoDB");
-}).catch((error) => {
+  })
+  .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
+  });
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to DB");
 });
 
-mongoose.connection.on('connected', () => {
-    console.log('Mongoose connected to DB');
+mongoose.connection.on("error", (err) => {
+  console.error(`Mongoose connection error: ${err}`);
 });
 
-mongoose.connection.on('error', (err) => {
-    console.error(`Mongoose connection error: ${err}`);
-});
-
-mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose disconnected');
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected");
 });
